c67dcdde1b269412a3f3a89210f6f4ec94010c08
Cin1983! <cinnymaria@gmail.com>
2020-10-31 19:28:30 +0100

    React tutorial 31 oktober 2020

---
+++ REACT/oefening/src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hyacinthia word wakker!</h1>
+    </div>
+  );
+}
+
+export default App;
